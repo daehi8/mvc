accc076ee976ebf1ed904d9284e2e5f8b706cb7f
Dae Hee <daehi8@naver.com>
2020-11-27 10:13:26 +0900

    member actionclass create and properties add

---
+++ src/mvc/model/ModifyAction.java
@@ -0,0 +1,14 @@
+package mvc.model;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ModifyAction implements SuperAction{
+
+	@Override
+	public String requestAction(HttpServletRequest request, HttpServletResponse response) {
+		
+		return "";
+	}
+
+}
---
+++ src/mvc/model/ModifyFormAction.java
@@ -0,0 +1,14 @@
+package mvc.model;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ModifyFormAction implements SuperAction{
+
+	@Override
+	public String requestAction(HttpServletRequest request, HttpServletResponse response) {
+		
+		return "";
+	}
+
+}
